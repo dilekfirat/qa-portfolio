df38c10e04ba8f623e861302b047f99fe9300d06
Ferhat FIRAT <ferhatfirat@Ferhat-MacBook-Air.local>
2026-01-03 11:58:30 +0100

    Add initial Playwright setup and tests

---
+++ automation/tests/view_product_details_page.spec.js
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test'; 
+
+test('View product details page', async ({page}) => {
+
+  await page.goto('https://www.saucedemo.com/');
+  await page.fill('[data-test="username"]', 'standard_user');
+  await page.fill('[data-test="password"]', 'secret_sauce');
+  await Promise.all([
+  page.waitForURL('**/inventory.html'),
+  page.click('[data-test="login-button"]')
+]);
+  await page.locator('[data-test="item-4-title-link"]').click();
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory-item.html?id=4');
+  await expect(page.locator('.inventory_details_name.large_size')).toHaveText('Sauce Labs Backpack');
+  // ---------------------
+})
