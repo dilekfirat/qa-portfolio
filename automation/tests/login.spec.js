8f3ecbbed3e12058dab2ccdf68b563c7cfc39205
dilekfirat <dilekfirat.qa@gmail.com>
2026-01-05 15:36:05 +0100

    updated login.spec with successful logout test

--- automation/tests/login.spec.js
+++ automation/tests/login.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 // Testing successful login
-test('login', async ({ page }) => {
+test('successful login with valid credentials', async ({ page }) => {
 
     await page.goto('https://www.saucedemo.com/');
     await expect(page).toHaveTitle(/Swag Labs/);
@@ -11,15 +11,25 @@ test('login', async ({ page }) => {
     await page.locator('[data-test="login-button"]').click();
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     await expect(page.locator('text=Products')).toHaveCount(1)
-    await page.close();
 
 });
 
 //Testing login with invalid credentials
+test('login fails with invalid username', async ({ page }) => {
 
+    await page.goto('https://www.saucedemo.com/');
+    await expect(page).toHaveTitle(/Swag Labs/);
+
+    await page.locator('[data-test="username"]').fill('user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/');
+    await expect(page.locator('text=Username and password do not match any user in this service')).toHaveCount(1)
+    
+});
 
 // Testing logout functionality 
-test('logout', async ({ page }) => {
+test('user can logout successfully', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await expect(page).toHaveTitle(/Swag Labs/);
     await page.locator('[data-test="username"]').fill('standard_user');
